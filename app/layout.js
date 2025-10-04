24833f1d5aaeb04e2df8d4c485b91988f36cbecb
Vaibhav Patil <vaibhavpatil2579@gmail.com>
2025-10-04 21:12:41 +0530

    Initial setup has done.

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Talk2Hire",
+  description: "Your AI voice coach for smarter interviews.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
