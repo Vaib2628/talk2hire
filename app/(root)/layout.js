e457e4ca4a949ac7fda2e0a318c2328cf6132c32
Vaibhav Patil <vaibhavpatil2579@gmail.com>
2025-10-07 20:44:29 +0530

    Sign Up and Login completed . Added the isAuthenticated checks in home page

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-const RootLayout = ({children}) => {
+import { isAuthenticated } from '@/lib/Actions/auth.action'
+import { redirect } from 'next/navigation'
+const RootLayout = async ({children}) => {
+  //check wheather the user is authenticated or not ?
+
+  const isUserAuthenticated = await isAuthenticated();
+
+  if(!isUserAuthenticated) {
+    redirect('/sign-in');
+  }
   return (
+
     <div className='root-layout'>
       <nav>
         <Link href={'/'} className='flex items-center gap-2'>
